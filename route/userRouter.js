f25820e2e2586b58c4ffd00b28465a7da8f24b70
Ashley Pavan Dsouza <vinusuhas2000@gmail.com>
2024-01-09 23:40:15 +0530

    fixed

--- route/userRouter.js
+++ route/userRouter.js
@@ -1,7 +1,9 @@
 const route = require("express").Router();
+const { paymentGatewall } = require("../controller/paymentController");
 const { register, login } = require("../controller/userController");
 
 route.post("/register", register);
 route.post("/login", login);
+route.post("/checkout",paymentGatewall)
 
 module.exports = { route };
